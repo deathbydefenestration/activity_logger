8565dd2db2eebff20820d3d60d59c3c7fc3027ba
Loki <deathbydefenestration@gmail.com>
2019-10-13 12:47:05 +0100

    Add User DB Model
    In order for an athlete to log an activity, there needs to a system user for the athlete.
    
    This commit ensures we're able to store a User, and assign them a type of user, such as an athlete.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from src import create_app
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
---
+++ backend/src/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+
+def create_app():
+    db.init_app(app)
+    app.app_context().push()
+
+    with app.app_context():
+        db.create_all()
+
+    return app
