37dbda373674d8fa0b44703f7e91ab189348f515
Loki <deathbydefenestration@gmail.com>
2019-10-13 15:10:41 +0100

    Add Athlete Model
    In order for an athlete to log an activity, there needs to be a notion of an athlete in the system.
    
    This commit ensures we set up the Athlete model, which can be linked back to the system User.

---
+++ backend/db_models/athlete.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, create_engine, ForeignKey, DECIMAL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+import config
+from db_models.user import User
+
+Base = declarative_base()
+
+
+class Athlete(Base):
+    """
+    Athlete Model
+
+    An Athlete record for a system User. These attributes pertain only to Athletes, i.e. weight (in kg)
+
+    """
+
+    __tablename__ = 'athletes'
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey(User.id), unique=True)
+
+    user = relationship(
+        User, primaryjoin=user_id == User.id, backref='athlete_user'
+    )
+
+    # Weight in kg
+    weight = Column(DECIMAL(5, 2))
+
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+
+Base.metadata.create_all(engine)
